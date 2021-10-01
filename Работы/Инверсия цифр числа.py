36daa32f25d36c1874c0e43650c83f0f02dbc770
Артём Горин <91244537+Gamatoto007@users.noreply.github.com>
2021-10-01 15:41:23 +0300

    Delete Работы directory

--- Работы/Инверсия цифр числа.py
+++
@@ -1,6 +0,0 @@
-x=input("Введите трёхзначное число ")
-a=int(x[0])
-b=int(x[1])
-c=int(x[2])
-a,c=c,a
-print(str(a)+str(b)+str(c))
--- Работы/Произведение цифр трёхзначного числа.py
+++
@@ -1,6 +0,0 @@
-x=input("Введите трехзначное число: ")
-a=int(x[0])
-b=int(x[1])
-c=int(x[2])
-S=a*b*c
-print(S)
