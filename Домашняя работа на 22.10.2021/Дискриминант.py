ad2c589ab9c8a15ace95dba1559d9e6c8f4b702d
Artom <yjvth071203@yandex.ru>
2021-10-21 00:31:50 +0300

    .

---
+++ Домашняя работа на 22.10.2021/Дискриминант.py
@@ -0,0 +1,13 @@
+a=int(input("Введите первый коэффициент "))
+b=int(input("Введите второй коэффициент "))
+c=int(input("Введите третий коэффициент "))
+def sum (a,b,c):
+    return ((b**2)-(4*a*c))
+D = sum (a,b,c)
+def biba (a,b,D):
+    return ((-b+((D)**(1/2)))/(2*a))
+Biba=biba(a,b,D)
+def boba (a,b,D):
+    return ((-b-((D)**(1/2)))/(2*a))
+Boba=boba(a,b,D)
+print(Biba,Boba)
