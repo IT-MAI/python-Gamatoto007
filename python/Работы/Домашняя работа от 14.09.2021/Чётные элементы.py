69544c776f16a96a0fe0b5a5747bd7ffc5c8712d
Artom <yjvth071203@yandex.ru>
2021-11-11 23:47:52 +0300

    .

--- python/Работы/Домашняя работа от 14.09.2021/Чётные элементы.py
+++ python/Работы/Домашняя работа от 14.09.2021/Чётные элементы.py
@@ -1,3 +1,5 @@
-N=int(input())
-for i in range (0,N,2):
-    print(i)
+q=input("Введите числовую последовательность: ")
+a=[int(q) for q in q.split()]
+for i in a:
+    if int(i) % 2 == 0:
+        print(i)
